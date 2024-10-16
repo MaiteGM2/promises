eb3559b3b78cb3849768a3d9ea80e4b2ef79cc96
maite <maite.sanchez@gm2dev.com>
2024-10-16 11:24:57 -0300

    feat(filedownload): add probability error method

--- script.js
+++ script.js
@@ -3,9 +3,15 @@ class FileDownload{
         this.name = name;
         this.size = size;
         this.downloadTime();
+        this.probabilityError();
     }
 
     downloadTime(){
         this.time = this.size * 10;
+    }
+
+    probabilityError(){
+        this.error = Math.random() > 0.3;
+        console.log(this.name,this.error);
     }
 }
