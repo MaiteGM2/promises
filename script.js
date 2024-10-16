75fc93c1f86d58a266e8c5af13faca73334c51bf
maite <maite.sanchez@gm2dev.com>
2024-10-16 11:41:39 -0300

    feat(script.js): add a promise.all of the files

--- script.js
+++ script.js
@@ -7,7 +7,7 @@ class FileDownload{
     }
 
     downloadTime(){
-        this.time = this.size * 10;
+        this.time = this.size * 0.1;
     }
 
     probabilityError(){
@@ -34,3 +34,11 @@ const file1 = new FileDownload(`VS Code`, 500);
 const file2 = new FileDownload(`Solid Edge`, 6500);
 const file3 = new FileDownload(`Spotify`, 1440);
 const file4 = new FileDownload(`EA APP`, 664);
+
+Promise.all([file1.download(), file2.download(), file3.download(), file4.download()])
+    .then((values) => {
+        console.log(`Success on all downloads:`, values);
+    })
+    .catch((error) => {
+        console.error(`There was a problem downloading at least one file:`, error);
+    });
