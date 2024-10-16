f077289626f8611881d663a82381a09ca8eca697
maite <maite.sanchez@gm2dev.com>
2024-10-16 11:24:17 -0300

    feat(filedownload): add download time method

--- script.js
+++ script.js
@@ -2,5 +2,10 @@ class FileDownload{
     constructor(name, size){
         this.name = name;
         this.size = size;
+        this.downloadTime();
+    }
+
+    downloadTime(){
+        this.time = this.size * 10;
     }
 }
