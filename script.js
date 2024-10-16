36dd033b277701085fd437699e65c1ef02673902
maite <maite.sanchez@gm2dev.com>
2024-10-16 11:25:26 -0300

    feat(filedownload): add download method

--- script.js
+++ script.js
@@ -14,4 +14,18 @@ class FileDownload{
         this.error = Math.random() > 0.3;
         console.log(this.name,this.error);
     }
+
+    download(){
+        return new Promise((resolve, reject) => {
+            console.log(`Starting download of ${this.name} file`);
+            setTimeout(() => {
+                if(this.error){
+                    resolve(`Successful download of ${this.name} file`);
+                } else{
+                    reject(`${this.name} file download failed`);
+                }
+        
+            }, this.time);
+        });
+    }
 }
